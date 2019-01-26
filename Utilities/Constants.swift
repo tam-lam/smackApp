e5260d8599786f8f57825bdc670faca6086dda40
Tam Lam <imtamtlam@gmail.com>
2019-01-26 11:22:23 +0700

    loginVC

---
+++ Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Graphene on 26/1/19.
+//  Copyright © 2019 Graphene. All rights reserved.
+//
+
+import Foundation
+ let TO_LOGIN = "toLogin"
---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,33 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Graphene on 26/1/19.
+//  Copyright © 2019 Graphene. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
