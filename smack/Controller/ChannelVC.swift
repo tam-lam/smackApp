53f0beb7645253e3608a8816673267261586ab4d
Tam Lam <imtamtlam@gmail.com>
2019-02-01 19:28:24 +0700

    channelCell

--- smack/Controller/ChannelVC.swift
+++ smack/Controller/ChannelVC.swift
@@ -11,12 +11,15 @@ import UIKit
 class ChannelVC: UIViewController {
     
     //IBOutlets
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var userImg: CircleImage!
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         self.revealViewController()!.rearViewRevealWidth = self.view.frame.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
@@ -48,4 +51,20 @@ class ChannelVC: UIViewController {
             userImg.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
         }
     }
+}
+extension ChannelVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell{
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configCell(channel: channel)
+            return cell
+        }
+        return ChannelCell()
+    }
+    
+    
 }
---
+++ smack/View/ChannelCell.swift
@@ -0,0 +1,32 @@
+//
+//  ChannelCell.swift
+//  smack
+//
+//  Created by Graphene on 1/2/19.
+//  Copyright © 2019 Graphene. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected{
+            self.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2)
+        } else{
+            self.layer.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        }
+    }
+    func configCell(channel: Channel){
+        let title = channel.channelTitle ?? ""
+        channelName.text = title
+    }
+
+}
